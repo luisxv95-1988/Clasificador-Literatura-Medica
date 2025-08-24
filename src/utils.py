51e902dee0aaa753deb0013b9748768be08144d3
Luis_Carlos <luisxv95@gmail.com>
2025-08-24 14:50:10 -0500

    Update utils.py
    Add Code

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,10 @@
-print('Funciones auxiliares...')
+import pandas as pd
+
+def load_data(path):
+    """Carga el dataset desde CSV con columnas title, abstract, group"""
+    df = pd.read_csv(path)
+    if not {"title", "abstract", "group"}.issubset(df.columns):
+        raise ValueError("El CSV debe contener las columnas: title, abstract, group")
+    df["text"] = df["title"].fillna("") + " " + df["abstract"].fillna("")
+    return df
+
